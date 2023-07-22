faca0ee23409e9ea9eebc87736673afcfd879bf9
tanaliga100 <jordantanaliga@gmail.com>
2023-07-22 22:19:47 +0800

    dataTypes-inputs-variables

--- variables-dataTypes-inputs.py
+++ variables-dataTypes-inputs.py
@@ -7,15 +7,28 @@ money = 100.990001
 isTall = False
 
 ## can only concatenaTe str to str || number to number
-# print(firstName +" "+lastName)
-# print(age)
-# print(money)
-# print(money + age)
+# # print(firstName +" "+lastName)
+# # print(age)
+# # print(money)
+# # print(money + age)
+#
+# ## USING INPUTS TO MAKE A INPUT BOX
+# # firstNameInput = input("Enter First Name :")
+# # lastNameInput = input("Enter a Last Name :")
+# # code = int(input("Enter your code :"))##
 
-firstNameInput = input("Enter First Name :")
-# lastNameInput = input("Enter a Last Name :")
-code = int(input("Enter your code :"))
+# print("HI", firstNameInput + str(code))
 
-print("HI", firstNameInput + str(code))
+firstSalary = "20.20"
+secondSalary = 25
+print("SALARY PROGRESSION :", float(firstSalary) + secondSalary)
+
+# ONE FUNCTION CALCULATOR
+
+firstNumber = int(input("Enter a first Number :"))
+secondNumber = int(input("Enter a second Number :"))
+result = firstNumber * secondNumber
+
+print(str(firstNumber) +"*"+ str(secondNumber) + "=" + str(result))
 
 

ac8098759807956a440e9d5beed94c5387e5ef3a
tanaliga100 <jordantanaliga@gmail.com>
2023-07-22 21:53:10 +0800

    first commit

---
+++ variables-dataTypes-inputs.py
@@ -0,0 +1,21 @@
+## DATA TYPES IN PYTHON
+
+firstName = "jordan"
+lastName = "tanaliga"
+age = 100
+money = 100.990001
+isTall = False
+
+## can only concatenaTe str to str || number to number
+# print(firstName +" "+lastName)
+# print(age)
+# print(money)
+# print(money + age)
+
+firstNameInput = input("Enter First Name :")
+# lastNameInput = input("Enter a Last Name :")
+code = int(input("Enter your code :"))
+
+print("HI", firstNameInput + str(code))
+
+
